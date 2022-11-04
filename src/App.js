e4a6c151420596d3e0abfcf1f70225f38f12e63f
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 13:41:51 +0000

    Create Signup form add styles to form, and connect to api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import NavBar from './components/NavBar';
-import {Container} from 'react-bootstrap'
-import styles from './App.module.css'
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import styles from "./App.module.css";
 import { Route, Switch } from "react-router-dom";
+import SignUpForm from "./pages/auth/SignUpForm";
+import "./api/axiosDefaults";
 function App() {
   return (
     <div className={styles.App}>
-    <NavBar />
-    <Container className={styles.Main}>
-      <Switch>
-        <Route exact path="/" render={() => <h1>Home page</h1>} />
-        <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-        <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
-        <Route render={() => <p>Page not found!</p>} />
-      </Switch>
-    </Container>
-  </div>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
