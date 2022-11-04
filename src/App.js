59fd9356bff5150c3dbdd7811fdab7529d390229
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 13:09:06 +0000

    Set up work space, folder structure and Add navbar/logo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import {Container} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
