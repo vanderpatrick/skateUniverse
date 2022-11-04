e90b6a60157e3837301311a9fa7708dd95ce46ad
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 13:43:20 +0000

    Change axios defaults base url

--- src/api/axiosDefaults.jsx
+++ src/api/axiosDefaults.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "skt-drf.herokuapp.com";
+axios.defaults.baseURL = "https://skt-drf.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
